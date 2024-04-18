cad2a22cc37df216f9d23cc731d90a2939ac99b4
TProgramer <tposter@naver.com>
2024-04-18 16:02:58 +0900

    :tada: Rank 서비스를 위한 Spring Boot App 초기 설정

---
+++ src/test/java/org/goorm/wordsketch/rank/RankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goorm.wordsketch.rank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
